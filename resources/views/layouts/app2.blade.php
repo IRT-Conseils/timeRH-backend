e2d4cf298e1fdc6da5ccc4ee648fb6669618cdf0
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2021-09-29 05:50:56 +0200

    added all navigation pages

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,28 +1,204 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
-    <script src="{{mix('js/app.js')}}" defer></script>
-    <title>Time RH | @yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Meta -->
+    <meta name="description" content="Time RH">
+    <meta name="author" content="IRT Conseils">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @yield('stylesheets')
+    <!-- vendor css -->
+    <link href="{{ asset('lib/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/jqvmap/jqvmap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/fullcalendar/fullcalendar.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/lib/select2/css/select2.min.css') }}" rel="stylesheet">
+
+    <!-- template css -->
+    <link rel="stylesheet" href="{{ asset('css/cassie.css') }}">
+
+    <!-- Styles -->
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+
 </head>
 <body>
-<div>
-    <nav>
-        <ul>
-            <li><a href="{{route('rh.dashboard')}}">Mon espace</a></li>
-            <li><a href="{{route('rh.my-requests')}}">Mes demandes</a></li>
-            <li><a href="{{route('rh.my-collaborators')}}">Collaborateurs</a></li>
-            <li><a href="{{route('rh.entries-and-validations')}}">Saisie et validation</a></li>
-            <li><a href="{{route('rh.temporary-workers')}}">Intérimaires</a></li>
-            <li><a href="{{route('rh.medical-check-up')}}">Visite médicales</a></li>
-            <li><a href="{{route('rh.e-document')}}">E-document</a></li>
+
+<div class="sidebar">
+    <div class="sidebar-header">
+        <div>
+            <a href="#" class="sidebar-logo"><span>Time RH</span></a>
+            <small class="sidebar-logo-headline">Votre compagnon RH</small>
+        </div>
+    </div><!-- sidebar-header -->
+    <div id="dpSidebarBody" class="sidebar-body">
+        <ul class="nav nav-sidebar">
+            <li class="nav-label"><label class="content-label">Paie et RH</label></li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="box"></i> Mon espace</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="layout"></i> Mes demandes</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="lock"></i> Collaborateurs</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="lock"></i> Saisie et
+                    Validation</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="x-circle"></i> Intérimaires</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i data-feather="user"></i>Visite médicale</a>
+            </li>
+
+                        <li class="nav-item">
+                                <a href="#" class="nav-link"><i data-feather="layers"></i> E-documents</a>
+                            </li>
         </ul>
-    </nav>
+
+        <hr class="mg-t-30 mg-b-25">
+
+        <ul class="nav nav-sidebar">
+            <li class="nav-item"><a href="#" class="nav-link"><i data-feather="aperture"></i> FAQ</a></li>
+            <li
+                class="nav-item"><a href="#" class="nav-link"><i
+                        data-feather="help-circle"></i>
+                    Mentions légales
+                </a></li>
+            <li
+                class="nav-item"><a href="#" class="nav-link"><i
+                        data-feather="help-circle"></i>
+                    Politique de confidentialité
+                </a></li>
+        </ul>
+
+
+    </div><!-- sidebar-body -->
+</div><!-- sidebar -->
+
+<div class="content">
+    <div class="header">
+        <div class="header-left">
+            <a href="" class="burger-menu"><i data-feather="menu"></i></a>
+
+                        <div class="header-search">
+                                <i data-feather="search"></i>
+                                <input type="search" class="form-control" placeholder="Qu'est-ce que vous cherchez?">
+                            </div><!-- header-search -->
+        </div><!-- header-left -->
+
+        <div class="header-right">
+            <a href="" class="header-help-link"><i data-feather="help-circle"></i></a>
+            <div class="dropdown dropdown-notification">
+                <a href="" class="dropdown-link new" data-toggle="dropdown"><i data-feather="bell"></i></a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-menu-header">
+                        <h6>Notifications</h6>
+                        <a href=""><i data-feather="more-vertical"></i></a>
+                    </div><!-- dropdown-menu-header -->
+                    <div class="dropdown-menu-body">
+                        <a href="" class="dropdown-item">
+                            <div class="avatar"><span class="avatar-initial rounded-circle text-primary bg-primary-light">s</span></div>
+                            <div class="dropdown-item-body">
+                                <p><strong>Socrates Itumay</strong> marked the task as completed.</p>
+                                <span>5 hours ago</span>
+                            </div>
+                        </a>
+                        <a href="" class="dropdown-item">
+                            <div class="avatar"><span class="avatar-initial rounded-circle tx-pink bg-pink-light">r</span></div>
+                            <div class="dropdown-item-body">
+                                <p><strong>Reynante Labares</strong> marked the task as incomplete.</p>
+                                <span>8 hours ago</span>
+                            </div>
+                        </a>
+                        <a href="" class="dropdown-item">
+                            <div class="avatar"><span class="avatar-initial rounded-circle tx-success bg-success-light">d</span></div>
+                            <div class="dropdown-item-body">
+                                <p><strong>Dyanne Aceron</strong> responded to your comment on this <strong>post</strong>.</p>
+                                <span>a day ago</span>
+                            </div>
+                        </a>
+                        <a href="" class="dropdown-item">
+                            <div class="avatar"><span class="avatar-initial rounded-circle tx-indigo bg-indigo-light">k</span></div>
+                            <div class="dropdown-item-body">
+                                <p><strong>Kirby Avendula</strong> marked the task as incomplete.</p>
+                                <span>2 days ago</span>
+                            </div>
+                        </a>
+                    </div><!-- dropdown-menu-body -->
+                    <div class="dropdown-menu-footer">
+                        <a href="">Voir toute les notifications</a>
+                    </div>
+                </div><!-- dropdown-menu -->
+            </div>
+            <div class="dropdown dropdown-loggeduser">
+                <a href="" class="dropdown-link" data-toggle="dropdown">
+                    <div class="">
+                        Bienvenue <strong>{{ Auth::user()->name }}</strong>
+                    </div><!-- avatar -->
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-menu-header">
+                        <div class="media align-items-center">
+                            <div class="media-body mg-l-10">
+                                <h6>{{ Auth::user()->name }}</h6>
+                                <span>Ingénieur système</span>
+                            </div>
+                        </div><!-- media -->
+                    </div>
+                    <div class="dropdown-menu-body">
+                        <a href="#" class="dropdown-item"><i data-feather="user"></i> Voir le profil</a>
+                                                <a href="#" class="dropdown-item"><i data-feather="edit-2"></i> Editer le Profile</a>
+                        <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}><i data-feather="log-out"></i> Se déconnecter</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div><!-- dropdown-menu -->
+            </div>
+        </div><!-- header-right -->
+    </div><!-- header -->
     @yield('content')
-</div>
+    <div class="content-footer text-center">
+        &copy; 2021. Tous droits réservés. Conception par <a href="https://www.irtconseils.fr/" target="_blank">IRT
+            Soft</a> &nbsp;partenaire <a href="#" target="_blank">IRT Conseils</a>.
+    </div><!-- content-footer -->
+</div><!-- content -->
+
+@yield('js')
+<script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('lib/jqueryui/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('lib/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('lib/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('lib/js-cookie/js.cookie.js') }}"></script>
+<script src="{{ asset('lib/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('lib/fullcalendar/fullcalendar.min.js') }}"></script>
+<script src="{{ asset('lib/select2/js/select2.full.min.js') }}"></script>
+<script src="{{ asset('lib/jquery.flot/jquery.flot.js') }}"></script>
+<script src="{{ asset('lib/jquery.flot/jquery.flot.stack.js') }}"></script>
+<script src="{{ asset('lib/jquery.flot/jquery.flot.resize.js') }}"></script>
+
+<script src="{{ asset('js/flot.sampledata.js') }}"></script>
+<script src="{{ asset('js/cassie.js') }}"></script>
+<script src="{{ asset('js/calendar-events.js') }}"></script>
+<script src="{{ asset('js/calendar.js') }}"></script>
+
 </body>
 </html>
--- resources/views/rh/my-requests.blade.php
+++ resources/views/rh/my-requests.blade.php
@@ -1,7 +1,213 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('title', 'Mes demandes RH')
 
 @section('content')
-    <h1>Mes demandes RH</h1>
+    <div class="content-header justify-content-between">
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="homepage.html">Accueil</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Mes demandes</li>
+                </ol>
+            </nav>
+            <h4 class="content-title content-title-xs">Mes demandes</h4>
+        </div>
+        <div class="d-none d-sm-flex">
+                        <a href="" class="btn btn-light"><i data-feather="print" class="svg-14"></i> Imprimer</a>
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle" type="button" id="ask-form" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Nouvelle demande
+                </button>
+                <div class="dropdown-menu tx-14" aria-labelledby="ask-form" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 37px, 0px); top: 0px; left: 0px; will-change: transform;">
+                    <a class="dropdown-item" href="#">Congés</a>
+                    <a class="dropdown-item" href="#">RTT</a>
+                    <a class="dropdown-item" href="#">Absence</a>
+                    <a class="dropdown-item" href="#">Arrêt maladie</a>
+                    <a class="dropdown-item" href="#">Demande RH</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle" type="button" id="filter-nature" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Filtre par nature
+                </button>
+                <div class="dropdown-menu tx-14" aria-labelledby="filter-nature" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 37px, 0px); top: 0px; left: 0px; will-change: transform;">
+                    <a class="dropdown-item" href="#">Congés</a>
+                    <a class="dropdown-item" href="#">RTT</a>
+                    <a class="dropdown-item" href="#">Absences</a>
+                    <a class="dropdown-item" href="#">Arrêts maladie</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle" type="button" id="filter-status" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Filtre par statut
+                </button>
+                <div class="dropdown-menu tx-14" aria-labelledby="filter-status" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 37px, 0px); top: 0px; left: 0px; will-change: transform;">
+                    <a class="dropdown-item" href="#">En cours</a>
+                    <a class="dropdown-item" href="#">Validé</a>
+                    <a class="dropdown-item" href="#">En attente</a>
+                    <a class="dropdown-item" href="#">refusées</a>
+                </div>
+            </div>
+        </div>
+    </div><!-- content-header -->
+    <div class="content-body">
+        <div class=" no-code">
+            <h5 id="section7" class="tx-semibold">Filtre par date</h5>
+            <p class="mg-b-25">Sélectionnez la plage de dates à rechercher.</p>
+
+            <div class="row">
+                <div class="col-3">
+                    <input type="date" id="dateFrom" class="form-control hasDatepicker" placeholder="Du">
+                </div><!-- col -->
+                <div class="col-3">
+                    <input type="date" id="dateTo" class="form-control hasDatepicker" placeholder="Au">
+                </div><!-- col -->
+            </div><!-- row -->
+        </div>
+
+        <div class="alert alert-success alert-dismissible fade show mt-5 mb-5" role="alert">
+            Votre demande à bien été prise en compte.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+
+        <div class="card mt-5 mb-2">
+            <div class="card-body">
+                <table class="table">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Nature</th>
+                        <th scope="col">Date de début</th>
+                        <th scope="col">Date de fin</th>
+                        <th scope="col">Durée</th>
+                        <th scope="col">Statut</th>
+                        <th scope="col">Commentaire</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">
+                            <div class="dropdown">
+                                <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Actions
+                                </button>
+                                <div class="dropdown-menu tx-14" aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 37px, 0px);">
+                                    <a class="dropdown-item" href="#">Réémettre</a>
+                                    <a class="dropdown-item" href="#">Imprimer</a>
+                                    <a class="dropdown-item" href="#">Annuler</a>
+                                    <a class="dropdown-item" href="#">Modifier</a>
+                                </div>
+                            </div>
+                        </th>
+                        <td>Titre requête</td>
+                        <td>24/09/21</td>
+                        <td>28/09/21</td>
+                        <td>4</td>
+                        <td>En cours</td>
+                        <td>-</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="mb-3">
+           Ici pagination si nécéssaire
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Mes demandes</h5>
+                <table class="table">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col">Intitulé</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Formulaire</th>
+                        <th scope="col">Etat</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">Titre</th>
+                        <td>24/02/21</td>
+                        <td>Montant</td>
+                        <td>En cours</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div><!-- content-body -->
+@endsection
+
+@section('js')
+
+    <script src="{{ asset('assets/js/flot.sampledata.js') }}"></script>
+    <script src="{{ asset('assets/js/vmap.sampledata.js') }}"></script>
+    <script src="{{ asset('assets/js/dashboard-one.js') }}"></script>
+
+    <script>
+        $(function(){
+            'use strict'
+
+            $('#datepicker1').datepicker();
+
+            $('#datepicker2').datepicker({
+                showOtherMonths: true,
+                selectOtherMonths: true
+            });
+
+            $('#datepicker3').datepicker({
+                showOtherMonths: true,
+                selectOtherMonths: true,
+                changeMonth: true,
+                changeYear: true
+            });
+
+            $('#datepicker4').datepicker();
+
+            $('#datepicker5').datepicker({
+                showButtonPanel: true
+            });
+
+            $('#datepicker6').datepicker({
+                numberOfMonths: 2
+            });
+
+            var dateFormat = 'mm/dd/yy',
+                from = $('#dateFrom')
+                    .datepicker({
+                        defaultDate: '+1w',
+                        numberOfMonths: 2
+                    })
+                    .on('change', function() {
+                        to.datepicker('option','minDate', getDate( this ) );
+                    }),
+                to = $('#dateTo').datepicker({
+                    defaultDate: '+1w',
+                    numberOfMonths: 2
+                })
+                    .on('change', function() {
+                        from.datepicker('option','maxDate', getDate( this ) );
+                    });
+
+            function getDate( element ) {
+                var date;
+                try {
+                    date = $.datepicker.parseDate( dateFormat, element.value );
+                } catch( error ) {
+                    date = null;
+                }
+
+                return date;
+            }
+
+
+        });
+    </script>
 @endsection
